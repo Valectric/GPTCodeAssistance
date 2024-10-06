8f91a447d5c7bff13d28f3b47aa857baddb61550
Johan Holtby <johan.holtby@valectric.se>
2024-10-06 23:26:39 +0200

    Red green test driven development implemented. Working on GPT conenction

---
+++ GPTCodeAssistance/GPT/GptConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GptCodeAssistant.GPT {
+    public class GptConnection {
+        // Method for sending a prompt
+        public string SendPrompt(string prompt) {
+            // For now, we return a mock non-empty response
+            return "This is a simulated response to: " + prompt;
+        }
+    }
+}
---
+++ GptCodeAssistantTests/GPT/GptConnectionTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using GptCodeAssistant.GPT;
+
+namespace GptCodeAssistant.Tests {
+    [TestFixture]
+    public class GptConnectionTests {
+        [Test]
+        public void SendPrompt_ShouldReturnNonEmptyString() {
+            // Arrange
+            var gptConnection = new GptConnection();
+            string prompt = "Test prompt";
+
+            // Act
+            string result = gptConnection.SendPrompt(prompt);
+
+            // Assert
+            Assert.That(result,Is.Not.Empty,"The result should not be empty");
+        }
+    }
+}
