30df01c6de1f32b225648308c6c270d47e66e90d
Johan Holtby <johan.holtby@valectric.se>
2024-10-15 04:38:02 +0200

    WIP fix the code errors in SK test due to referance missing

--- GPTCodeAssistanceTests/SK/SemanticKernelManagerTests.cs
+++ GPTCodeAssistanceTests/SK/SemanticKernelManagerTests.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace GPTCodeAssistanceTests.SK {
     internal class SemanticKernelManagerTests {
+        [Test]
+        public void SemanticKernelManager_ShouldInitializeKernel_OnCreation() {
+            // Arrange & Act
+            var kernelManager = new SemanticKernelManager();
+
+            // Assert
+            Assert.That(kernelManager.Kernel,Is.Not.Null,"The Kernel instance should be initialized on creation.");
+        }
 
     }
 }
